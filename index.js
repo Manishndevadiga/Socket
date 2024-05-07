077ba9958a68bc93dcf433e47619fb1cdd588ce6
Manish N Devadiga <gdevadiga109@gmail.com>
2024-05-07 10:00:27 +0530

    added /chat route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require("path");
 const { Server } = require("socket.io");
 const liveChat = require("./liveChat");
+const fs = require("fs");
 
 const server = http.createServer(app);
 
@@ -16,6 +17,20 @@ liveChat(io);
 
 app.get("/", (req, res) => {
   return res.sendFile(path.resolve("./public/second.html"));
+});
+
+app.get("/chats", (req, res) => {
+  return res.sendFile(path.resolve("./public/chats.html"));
+});
+
+app.get("/getChats", (req, res) => {
+  fs.readFile("messages.txt", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Error reading file");
+    }
+    return res.status(200).send(data);
+  });
 });
 
 server.listen(3000, () => {
--- liveChat.js
+++ liveChat.js
@@ -1,10 +1,30 @@
+const fs = require("fs");
+
 const liveChat = (io) => {
   io.on("connection", (socket) => {
     socket.on("user-message", (data) => {
       console.log(data);
-      io.emit("client-message", data);
+      let ipAddress = socket.request.connection.remoteAddress;
+      console.log(ipAddress);
+      if (data && data.trim() !== "") {
+        storeMessage(data);
+        io.emit("client-message", data);
+      } else {
+        io.emit("client-message", "Please enter valid message");
+      }
     });
   });
 };
+
+function storeMessage(data) {
+  const message = JSON.stringify(data) + "\n";
+  fs.appendFile("messages.txt", message, (err) => {
+    if (err) {
+      console.error("Error storing message:", err);
+    } else {
+      console.log("Message stored successfully.");
+    }
+  });
+}
 
 module.exports = liveChat;
